f66a987de7bd87bbd7a91c1c4b116761b7e6efe7
OwenP04 <opalukaitis@gmail.com>
2025-04-21 20:27:16 +0000

    version 1

--- variables.tf
+++ variables.tf
@@ -1,29 +1,21 @@
 variable "resource_group_name" {
-  description = "813-007a78e9-hands-on-with-terraform-on-azure"
-  type        = string
-  default     = "my-resource-group"
+  type = string
 }
 
 variable "location" {
-  description = "Azure region for the storage account"
-  type        = string
-  default     = "eastus"
+  type = string
 }
 
 variable "storage_account_name" {
-  description = "Name of the Azure storage account (must be globally unique, 3-24 lowercase letters and numbers)"
-  type        = string
-  default     = "mystorageaccount123" # Replace with a unique name
+  type = string
 }
 
 variable "account_tier" {
-  description = "Tier of the storage account (Standard or Premium)"
-  type        = string
-  default     = "Standard"
+  type    = string
+  default = "Standard"
 }
 
 variable "account_replication_type" {
-  description = "Replication type for the storage account (LRS, GRS, RAGRS, ZRS, etc.)"
-  type        = string
-  default     = "LRS"
+  type    = string
+  default = "GRS"
 }
